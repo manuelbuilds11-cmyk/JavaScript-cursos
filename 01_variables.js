335a9bb9ce394440532ebee1be8615a8223807c6
Manuel <manuelbuidls11@gmail.com>
2026-08-12 12:09:51 +0200

    Update 1

--- 01_variables.js
+++ 01_variables.js
@@ -3,6 +3,16 @@ var helloWorld = "Hello JavaScript"
 console.log(helloWorld)
 helloWorld = "Hola de nuevo, JavaScript"
 console.log(helloWorld)
+let helloWorld2=  "Hello JavaScrpit2"
+console.log(helloWorld2)
+ helloWorld2= "Hola de nuevo JavaScript 2"
+console.log(helloWorld2)
+let helloWorld3 = "Hello JavaScript3"
+console.log(helloWorld3)
+ helloWorld3 = "Hola de nuevo JavaScript 3"
+console.log(helloWorld3)
+const helloWorld4  = "Mi primera constante"
+console.log(helloWorld4)
+//let: si tiene que ir variando a constante
+//const: si es una constante invariable y fija
 
-//let
-//const
---
+++ 02-datatypes.js
@@ -0,0 +1,37 @@
+// Tipos de datos primitivos en Js
+//String: cadena de texto
+let name = "Johan"
+let alias = `Manuel`
+//Números
+let age = 18 //Entero
+let altura = 1.78 //Decimal
+//Boleanos (bolean): true or false
+let isStudent = true
+let isTeacher = false
+let isChild = false
+let isAdult = true
+//Undefined
+let UndefinedValue
+console.log(UndefinedValue)
+//null
+let nullvalue = null
+//Symbol
+let mySymbol = Symbol("mysymbol")
+//Bigint
+let myBigint = BigInt(723727987956879057850978590678742834738467906456567)
+let myBigint2 = 723727987956879057850978590678742834738467906456567n
+//Mostrar tipos de datos
+console.log(typeof name)
+console.log(typeof alias)
+console.log(typeof age)
+console.log(typeof altura)
+console.log(typeof isStudent)
+console.log (typeof isTeacher)
+console.log(typeof isChild)
+console.log(typeof isAdult)
+console.log(typeof UndefinedValue)
+console.log(typeof mySymbol)
+console.log(typeof myBigint)
+console.log(typeof myBigint2)
+console.log(typeof nullvalue)
+
