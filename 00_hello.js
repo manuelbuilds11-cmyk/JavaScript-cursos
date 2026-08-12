2ed53e2cb76bf8b0a6054b5a4653ac6e61ac91dc
Manuel <manuelbuidls11@gmail.com>
2026-08-12 10:27:23 +0200

    first commit

---
+++ 00_hello.js
@@ -0,0 +1,17 @@
+console.log("Hola JavaScript")
+console.log("Estoy aprendiendo a programar")
+//console.log('Comillas simples también')
+//Esto es una prueba de un comentario
+/*Esto es
+un comentario
+en varias líneas*/
+// EStas doslíneas al principiode una orden se utilizan también para anular líneas de código
+/*console.log("Hola JavaScript")
+console.log("Hola JavaScript")*/
+console.log(5)
+console.log(5+5)
+console.log(5-7)
+console.log(5*5)
+console.log(5/5)
+console.log(5%2)
+console.log(5**2)
---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+//var
+var helloWorld = "Hello JavaScript"
+console.log(helloWorld)
+helloWorld = "Hola de nuevo, JavaScript"
+console.log(helloWorld)
+
+//let
+//const
