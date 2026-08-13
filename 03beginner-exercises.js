7211fe7a070b766133e747441cd6399ad8112a29
Manuel <manuelbuidls11@gmail.com>
2026-08-13 14:45:29 +0200

    Completados ejercicios de variables, tipos de datos y constantes

---
+++ 03beginner-exercises.js
@@ -0,0 +1,58 @@
+//Este es un comentario simple, para que no se ejecute en la terminal
+/*Este es un comentario
+en varias
+líneas*/
+let name = "Pepe"
+let age = (30)
+let isStudent = (true)
+let address = (null)
+let email = (undefined)
+let id = (8754857398745389795n)
+let home = (Symbol("home"))
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(address)
+console.log(email)
+console.log(id)
+console.log(home)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+console.log(typeof address)
+console.log(typeof email)
+console.log(typeof id)
+console.log(typeof home)
+//Sin cambiar tipo de dato, cambia el valor de las variables
+name = "Manuel"
+ age = 25
+ isStudent = false
+ address = null
+email = undefined
+ id = 123456789n
+ home = Symbol("home")
+//Cambia el valor de las variables y su tipo de dato
+name = 123
+age = "cuarenta"
+isStudent = "nop"
+address = "Calle Falsa 234"
+email = "manuelexample@gmail.com"
+id = true
+home = 2873189793n
+const myname = "Ramón"
+const myage= 35
+const isstudent = false
+const myaddress =  null
+const myemail = undefined
+const myid = 98298742389n
+const myhome = Symbol("casa")
+/*myname = "Juan"
+myage = 40
+isstudent = true
+myaddress = "Calle Verdadera 123"
+myemail = "juanexample@gmail.com"
+myid = true
+myhome = 987654321n*/
+
+
+
---
+++ 04-operators.js
@@ -0,0 +1,35 @@
+//Operadores
+
+//Operadores aritméticos 
+let a = 10 
+let b = 5
+console.log(a+b) //Suma
+console.log(a-b) //Resta
+console.log(a*b) //Multiplicación
+console.log(a/b) //División
+console.log(a**b) //Exponente
+console.log(a%b) //Módulo
+a++ //Incremento
+console.log(a)
+b-- //Decremento
+console.log(b)
+//OPeradores de asignación
+let myVariable = 1
+console.log(myVariable)
+myVariable += 5 //myVariable = myVariable + 5
+console.log(myVariable)
+myVariable -= 2 //myVariable = myVariable - 2
+myVariable *= 1
+myVariable /= 2
+console.log(myVariable)
+
+//Operadores de comparación
+
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a==b) 
+console.log(a=a)
+console.log(a===5)
+
