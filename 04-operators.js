6fd1b085c13f8468ad6e8f43048322e5534c9d57
Manuel <manuelbuidls11@gmail.com>
2026-08-14 12:02:54 +0200

    Sesión Mouredev: operadores y nuevas notas

--- 04-operators.js
+++ 04-operators.js
@@ -31,5 +31,65 @@ console.log(a>=b)
 console.log(a<=b)
 console.log(a==b) 
 console.log(a=a)
-console.log(a===5)
+console.log(a==11) //igualdad por valor
+console.log(a=="11")//igualdad por valor
+console.log(a===11) //igualdad por valor y tipo
+console.log(a==="11")
+console.log(a!=11)
+console.log(a!=="11")
+console.log(0==false)
+console.log(1==false)
+console.log(2==false)
+console.log(0=="")
+console.log(0==" ")
+console.log(0==``)
+console.log(0==null)
+console.log(0==undefined)
+console.log(0=="Hola")
+console.log(0==="")
+console.log(null==undefined)
+console.log(null===undefined)
+//Truthy values (valores que se consideran verdaderos)
+//Todos los números positivos y negativos menos el 0
+//Todas las cadenas de texto menos las vacías
+// Los booleanos true
+
+//Falsy values (valores que se consideran falsos)
+//El número 0
+//Cadenas de texto vacías
+//0n
+//null
+//undefined
+//Nan
+// LOs booleanos flase
+
+//Operadores lógicos
+//and (&&)
+console.log(5>10 && 15>20)
+console.log(5<10 && 15>20)
+console.log(5>10 && 15<20)
+console.log(5>10 && 15<20 && 35>5)
+
+//or (||)
+console.log(5>10 || 15>20)
+console.log(5<10 || 15>20)
+console.log(5>10 || 15<20)
+console.log(5>10 || 15<20 || 35>5)
+console.log(5<10 || 15<20 || 35>5)
+console.log(5>10 && 15>20 || 30<40)
+
+//not (!)
+console.log(!(5>10 && 15>20))
+console.log(!(5>10 && 15>20 || 30<40))
+console.log(!true)
+console.log(!false)
+//Operadores ternarios
+const isRaining = false
+isRaining ? console.log("Lleva paraguas") : console.log("No lleva paraguas")
+
+
+
+
+
+
 
