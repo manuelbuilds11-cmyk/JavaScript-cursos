47193856c3c033821d97ccab5d71e74831e6db40
Manuel <manuelbuidls11@gmail.com>
2026-08-15 14:29:28 +0200

    Ejercicios prácticos completados: operadores aritméticos, de comparación, lógicos y ternarios

---
+++ 05-operators exercise.js
@@ -0,0 +1,67 @@
+//1 Crear una variable para cada operación aritmética
+let a = 10
+let b = 5
+let resultadoSuma = a + b
+let resultadoResta = a - b
+let resultadoMultiplicacion = a * b
+let resultadoDivision = a / b
+let resultadoPotencia = a ** b
+let resultadoModulo = a % b
+console.log(resultadoSuma)
+console.log(resultadoResta)
+console.log(resultadoMultiplicacion)
+console.log(resultadoDivision)
+console.log(resultadoPotencia)
+console.log(resultadoModulo)
+
+// 2. Crea una variable para cada tipo de operación de asignación, que haga uso de las variables utilizadas para las operaciones aritméticas
+ let Suma =1
+ let Resta=1
+ let Multiplicacion=1
+ let Division=1
+ let Potencia=1
+ let Modulo=1
+ console.log(Suma +=5)
+ console.log(Resta -=5)
+ console.log(Multiplicacion *=5)
+ console.log(Division /=5)
+ console.log(Potencia **=5)
+ console.log(Modulo %=5)
+ // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+ console.log(10<20)
+ console.log(10>5)
+ console.log(7===7)
+ console.log(10>=5)
+ console.log(10!=="10")
+ // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+ console.log(10>20)
+ console.log(10<5)
+console.log(1>=4)
+console.log(10===20)
+console.log(10!==10)
+// 5. Utiliza el operador lógico and
+console.log(10<20 && 5>3)
+// 6. Utiliza el operador lógico or
+console.log(10<5 || 6>2)
+// 7. Combina ambos operadores lógicos
+console.log(10>20 && 5<3 || 20>1)
+// 8. Añade alguna negación
+console.log(!(10>5 && 5>3 || 2<6))
+// 9. Utiliza el operador ternario
+const edad=18
+const mensaje = edad >=18 ? "Es mayor de edad": "ES menor de edad"
+console.log(mensaje)
+// 10. Combina operadores aritméticos, de comparación y lógicos
+const Aparato1 = 10
+const Aparato2 = 5
+console.log (Aparato1>Aparato2)
+const  Prueba = Aparato1>Aparato2 ? "True" : "False"
+console.log (Prueba)
+const Ejercicio = (Aparato1-5===Aparato2 && Aparato2+5===Aparato1) ? "True" : "False"
+console.log(Ejercicio)
+
+
+
+
+
+ 
