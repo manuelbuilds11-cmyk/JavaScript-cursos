4f67a3194dec394fa3ccda2d7ae9bc4dbaab3e87
Manuel <manuelbuidls11@gmail.com>
2026-08-17 12:18:53 +0200

    Strings exercises and operators types

---
+++ 07-strings exercise.js
@@ -0,0 +1,55 @@
+// 1. Concatena dos cadenas de texto
+ let Petit =  ("hola que tal")
+console.log ("Manuel, " + Petit)
+// 2. Muestra la longitud de una cadena de texto
+const email = ("manuel.example@gmail.com")
+console.log (email.length)
+// 3. Muestra el primer y último carácter de un string
+console.log(email[0])
+console.log(email[24])
+// 4. Convierte a mayúsculas y minúsculas un string
+
+let direction = ("MyDirectionExample123")
+console.log(direction.toUpperCase())
+console.log(direction.toLowerCase())
+// 5. Crea una cadena de texto en varias líneas
+let message = `Esto es un comentario
+en varias
+líneas`
+console.log(message)
+// 6. Interpola el valor de una variable en un string
+let resplandeciente = ("eres muy guapo")
+console.log(`Hola Manuel, ${resplandeciente}`)
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+const frase = (" Aprender Javascript es divertido ")
+console.log(frase.replaceAll(" ","-"))
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+console.log(frase.includes("Javascript"))
+// 9. Comprueba si dos strings son iguales
+let queen = ("mujer con corona")
+let king = ("hombre con corona")
+console.log(queen===king)
+// 10. Comprueba si dos strings tienen la misma longitud
+let Email1 = ("manuelexample@gmail.com")
+let Email2 = ("carlos.sample@gmail.com")
+console.log(Email1.length===Email2.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 08-conditionals.js
@@ -0,0 +1,18 @@
+//if,else if. else
+let age = (37)
+if (age==37){console.log("Tienes 37 años")}
+
+//else (si no)
+else {console.log("No tienes 37 años")}
+
+//else if (si no ,si)
+let edad = (12)
+if (edad==37){console.log("Tienes 37 años")}
+else if (edad<18){console.log("Eres menor de edad")}
+else if (edad>18){console.log("Eres mayor de edad")}
+else {console.log("No tienes 37 años")}
+
+//Operador ternario
+const message = age==37 ? "La edad es 37":"La edad no es 37"
+console.log(message)
+
